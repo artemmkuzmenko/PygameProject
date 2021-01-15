4529899ada4b4c49bbd22cc6cd0b5c7081602bc4
m-kuz <fine.arteom@yandex.ru>
2021-01-15 19:19:05 +0300

    Добавил файл  с классом для поля

---
+++ board.py
@@ -0,0 +1,23 @@
+import pygame
+
+
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self, screen):
+        for i in range(self.width):
+            for j in range(self.height):
+                pygame.draw.rect(screen, pygame.Color('white'),
+                                 ((self.left + self.cell_size * i, self.top + self.cell_size * j),
+                                  (self.cell_size, self.cell_size)), 1)
